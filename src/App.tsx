4219f1d75d21ed0b870831604346ae43cb9be23c
AlbertLnz <albert2000_lanza@hotmail.com>
2024-07-08 11:39:11 +0200

    Mapbox working fine w/ accessToken

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Example from './components/Example'
+import mapboxgl from 'mapbox-gl'
+import { useEffect, useRef } from 'react'
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN!
 
 const App = () => {
+  const mapContainerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return
+
+    new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/outdoors-v12',
+      center: [2.3878241365316, 42.12541767992249],
+      zoom: 12,
+      pitch: 60,
+      bearing: -60,
+      antialias: true,
+      attributionControl: false,
+    })
+  }, [])
+
   return (
-    <div>
-      <Example />
-    </div>
+    <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }}></div>
   )
 }
 
--- src/components/Example.tsx
+++
@@ -1,9 +0,0 @@
-const Example = () => {
-  return (
-    <div>
-      <h1>Example</h1>
-    </div>
-  )
-}
-
-export default Example
